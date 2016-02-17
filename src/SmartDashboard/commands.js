b28fd6e6d49117bf87aa23cdf887acf377a5e216
White Bread <thewhitebreadman@gmail.com>
2016-02-16 22:07:31 -0500

    Added code for webcam in smart dashboard

--- src/SmartDashboard/commands.js
+++ src/SmartDashboard/commands.js
@@ -1,14 +1,37 @@
 $(document).ready(function() {
 
-function getLeftSpeed()
+function getSpeed()
 {
-	$('#left-speed').val(NetworkTables.getValue('/SmartDashboard/leftSpeed', 0.0))
+	var left = NetworkTables.getValue('/SmartDashboard/talon0', 0.0)
+	var right = NetworkTables.getValue('/SmartDashboard/talon1', 0.0)
+	$('#speed').val((left + right) / 2.);
 }
 
-function getRightSpeed()
+function getDirection()
 {
-	$('#right-speed').val(NetworkTables.getValue('/SmartDashboard/rightSpeed', 0.0))
-	console.log($('#right-speed').val())
+	var left = NetworkTables.getValue('/SmartDashboard/talon0', 0.0)
+	var right = NetworkTables.getValue('/SmartDashboard/talon1', 0.0)
+	
+	if ((left == 0.0) && (right == 0.0))
+	{
+		$('#direction').val("Still ")
+		return
+	}
+	else if ((left > 0.0) && (right > 0.0))
+		$('#direction').val("Forward ")
+	else if ((left < 0.0) && (right < 0.0))
+		$('#direction').val("Backward ")
+		
+	if (Math.abs((left - right)/(left + right)) < .10)
+		$('#direction').val($('#direction').val() + "Straight")
+	else if (((left - right)/(left + right)) < .30)
+		$('#direction').val($('#direction').val() + "Slightly Left");
+	else if (((left - right)/(left + right)) >= .30)
+		$('#direction').val($('#direction').val() + "Left");
+	else if (((right - left)/(left + right)) < .30)
+		$('#direction').val($('#direction').val() + "Slightly Right");
+	else if (((right - left)/(left + right)) >= .30)
+		$('#direction').val($('#direction').val() + "Right");
 }
 
 function getLoadSpeed()
@@ -22,8 +45,8 @@ function getShootSpeed()
 }
 
 setInterval(function() {
-	getLeftSpeed();
-	getRightSpeed();
+	getSpeed();
+	getDirection();
 	getLoadSpeed();
 	getShootSpeed();
 }, 20);

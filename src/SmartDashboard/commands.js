00d64cb15fb7c9b6d9bc7cbb01aa80ac1c3fafd3
White Bread <thewhitebreadman@gmail.com>
2016-03-01 11:27:55 -0500

    Added Smart Dashboard

--- src/SmartDashboard/commands.js
+++ src/SmartDashboard/commands.js
@@ -1,14 +1,50 @@
 $(document).ready(function() {
 
-function getLeftSpeed()
+
+function getGear()
 {
-	$('#left-speed').val(NetworkTables.getValue('/SmartDashboard/leftSpeed', 0.0))
+	document.getElementById("robot-gear").style.fontSize = "24px"
+	var v = NetworkTables.getValue('/SmartDashboard/highGear', false)
+	
+	if (v)
+		document.getElementById("robot-gear").style.color = "green"
+	else
+		document.getElementById("robot-gear").style.color = "red"
+	$('#robot-gear').val((v ? "HIGH GEAR" : "LOW GEAR"))
 }
 
-function getRightSpeed()
+function getSpeed()
 {
-	$('#right-speed').val(NetworkTables.getValue('/SmartDashboard/rightSpeed', 0.0))
-	console.log($('#right-speed').val())
+	var left = NetworkTables.getValue('/SmartDashboard/talon0', 0.0)
+	var right = NetworkTables.getValue('/SmartDashboard/talon1', 0.0)
+	$('#speed').val((left + right) / 2.);
+}
+
+function getDirection()
+{
+	var left = NetworkTables.getValue('/SmartDashboard/talon0', 0.0)
+	var right = NetworkTables.getValue('/SmartDashboard/talon1', 0.0)
+	
+	if ((left == 0.0) && (right == 0.0))
+	{
+		$('#direction').val("Still ")
+		return
+	}
+	else if ((left > 0.0) && (right > 0.0))
+		$('#direction').val("Forward ")
+	else if ((left < 0.0) && (right < 0.0))
+		$('#direction').val("Backward ")
+		
+	if (Math.abs((left - right)/(left + right)) < .10)
+		$('#direction').val($('#direction').val() + "Straight")
+	else if (((left - right)/(left + right)) < .30)
+		$('#direction').val($('#direction').val() + "Slightly Left");
+	else if (((left - right)/(left + right)) >= .30)
+		$('#direction').val($('#direction').val() + "Left");
+	else if (((right - left)/(left + right)) < .30)
+		$('#direction').val($('#direction').val() + "Slightly Right");
+	else if (((right - left)/(left + right)) >= .30)
+		$('#direction').val($('#direction').val() + "Right");
 }
 
 function getLoadSpeed()
@@ -22,8 +58,9 @@ function getShootSpeed()
 }
 
 setInterval(function() {
-	getLeftSpeed();
-	getRightSpeed();
+	getGear();
+	getSpeed();
+	getDirection();
 	getLoadSpeed();
 	getShootSpeed();
 }, 20);
